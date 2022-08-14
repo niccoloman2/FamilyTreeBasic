7bfe3b0a0cc012e641f4b29cbd041229f7178ac6
Niccolo Manahan <niccolo.manahan@gmail.com>
2022-08-14 21:30:42 +0800

    Changed test server response

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({message: "Server test response"});
+    res.json({message: "Yo, welcome"});
 });
 
 app.listen(PORT, ()=>{
